07dec96082384a0b865695f1c280c48232dc8559
Chaerimy0907 <qkrcofus1026@daum.net>
2025-08-14 16:12:02 +0900

    기본 코드

--- src/tests/yolo_test_img.py
+++ src/tests/yolo_test_img.py
@@ -2,3 +2,13 @@
 opencv로 테스트했을 때 안전모가 아닌 부분까지 감지되어
 yolo를 사용해 조금 더 정밀한 감지 테스트
 '''
+from ultralytics import YOLO
+import cv2
+
+def detect_yellow_hardhat(img_path):
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    detect_yellow_hardhat('./img/1.jpg')
