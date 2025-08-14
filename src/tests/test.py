3ff91d9a9883eaf4487cc29d629ecd95ecb99672
Chaerimy0907 <qkrcofus1026@daum.net>
2025-08-14 15:12:14 +0900

    test 기본 코드 작성

---
+++ src/tests/test.py
@@ -0,0 +1,18 @@
+'''
+1일차 목표 : 웹캠으로 노란색 안전모 감지하여 착용여부 표시
+색상 범위 HSV 기준
+면적 필터링 : 노이즈 제거
+'''
+import cv2
+import numpy as np
+
+# 노란색 안전모 감지 함수
+def detect_yellow_hardhat(frame):
+
+# 감지 실행 함수
+def detection():
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__=="__main__":
